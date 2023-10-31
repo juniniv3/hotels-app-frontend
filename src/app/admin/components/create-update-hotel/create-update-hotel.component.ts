40c3446538d49cb6dca446c2606af5b5dd4e0174
Junini Florez <juniniprogramador@outlook.com>
2023-10-30 22:16:37 -0500

    feat: added img input file and others in the create hotel form

--- src/app/admin/components/create-update-hotel/create-update-hotel.component.ts
+++ src/app/admin/components/create-update-hotel/create-update-hotel.component.ts
@@ -1,23 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
 import { Location } from 'src/app/domain/location/interface/location';
+import { Service } from 'src/app/domain/service/interface/service';
 import { LocationService } from 'src/app/services/location/location.service';
+import { ServiceService } from 'src/app/services/service/service.service';
 
 @Component({
   selector: 'app-create-update-hotel',
   templateUrl: './create-update-hotel.component.html',
 })
 export class CreateUpdateHotelComponent implements OnInit {
-  constructor(private locationService: LocationService){}
-  locations: Location [] = [];
+  constructor(
+    private locationService: LocationService,
+    private serviceService: ServiceService,
+    private cd: ChangeDetectorRef
+  ) {}
+
+  locations: Location[] = [];
+  services: Service[] = [];
+  previewImgURL: string = '';
+
   ngOnInit(): void {
-    this.getLocations();
+    this.getBaseInfo();
   }
-  getLocations(){
-    this.locationService.getLocations().subscribe(
+  getBaseInfo() {
+    forkJoin([this.locationService.getLocations(),this.serviceService.getServices()]).subscribe(
       res => {
-        this.locations = res;
-        console.log(res);
+        this.locations =  res[0];
+        this.services = res[1];
       }
-    );
+    )
+  }
+
+  showPreview(event: any){
+    if(event.target.files.length > 0){
+      const src = URL.createObjectURL(event.target.files[0]);
+      const preview : HTMLElement | any = document.getElementById("file-preview");
+      if (preview){
+        this.previewImgURL = src;
+      }
+    }
+  }
+
+  clickLoadImage(){
+    const fileLoaderInput = document.getElementById("file-loader");
+    if (fileLoaderInput) {
+      fileLoaderInput.click();
+    }
   }
 }
